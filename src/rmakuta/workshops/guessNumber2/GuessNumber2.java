0dc94bd8d8ca515452f25ad477825d5e353489d9
Rafał Makuta <rafalmakuta@o2.pl>
2019-09-24 14:38:27 +0200

    GuessNumber [2]

--- src/rmakuta/workshops/guessNumber2/GuessNumber2.java
+++ src/rmakuta/workshops/guessNumber2/GuessNumber2.java
@@ -6,11 +6,56 @@ public class GuessNumber2 {
     public static void main(String[] args) {
 
         System.out.println("Pomyśl liczbę od 0 do 1000 a ja ją zgadnę w maksymalnie 10 próbach.");
-        Scanner scan = new Scanner(System.in);
 
         int min = 0, max = 1000, guess;
-        guess = ((max - min)/2) + min;
-        System.out.println("Zgaduję: " + guess);
-        
+        boolean next = true;
+        String answer;
+
+        Scanner scan = new Scanner(System.in);
+
+        while (next){
+
+//            guess = ((max - min)/2) + min;
+//
+//            System.out.println("Zgaduję: " + guess + "Zgadłem? (t/n)");
+//            answer = scan.nextLine();
+//
+//            if(answer.equals("t")){
+//                System.out.println("Wygrałem!");
+//                next = false;
+//            }else {
+//                System.out.println("Za dużo? (t/n)");
+//                answer = scan.nextLine();
+//                if(answer.equals("t")){
+//                    max = guess;
+//                }else {
+//                    System.out.println("Za mało? (t/n)");
+//                    answer = scan.nextLine();
+//                    if(answer.equals("t")){
+//                        min = guess;
+//                    }else {
+//                        System.out.println("Nie oszukuj!");
+//                    }
+//                }
+//            }
+
+            guess = ((max - min)/2) + min;
+
+            System.out.println("Zgaduję: " + guess + " (za dużo/za mało/zgadłeś)");
+            answer = scan.nextLine();
+
+            switch (answer){
+                case "za dużo":
+                    max = guess;
+                    break;
+                case "za mało":
+                    min = guess;
+                    break;
+                case "zgadłeś":
+                    System.out.println("Wygrałem!");
+                    next = false;
+                    break;
+            }
+        }
     }
 }
